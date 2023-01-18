d9b30a901fdd7b45e697b27812030ec0309efeac
Cesscoo <martinhernandez.fj@gmail.com>
2023-01-18 17:55:53 +0000

    Finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TaskList from "./taskList.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,19 +7,15 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid p-0 FondoFran ">
+			<div className="row mt-4 mb-3">
+				<div className="col-12">
+					<h1 className="text-center">
+						TODO LIST
+					</h1>
+					<TaskList/>
+				</div>
+			</div>
 		</div>
 	);
 };
